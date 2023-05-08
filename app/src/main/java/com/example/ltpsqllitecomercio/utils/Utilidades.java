aa18bfb7b84c54328aab135b6a124db553cc6b4b
LuisT0p20 <luistoribiopalacios@gmail.com>
2023-05-08 16:12:12 -0500

    CAMBIO EN TABLAS

--- app/src/main/java/com/example/ltpsqllitecomercio/utils/Utilidades.java
+++ app/src/main/java/com/example/ltpsqllitecomercio/utils/Utilidades.java
@@ -3,11 +3,16 @@ package com.example.ltpsqllitecomercio.utils;
 public class Utilidades {
 
     //contiene el listado de constantes globales al app
+    //tabla usuario
     public static final String TABLA_USUARIO = "usuario";
     public static final String CAMPO_ID = "id";
     public static final String CAMPO_NOMBRE = "nombre";
     public static final String CAMPO_TELEFONO = "telefono";
     public static final String CAMPO_ID_FORMA_CONTACTO = "idFormaContacto";
+    //tabla forma contacto
+    public static final String TABLA_FORMA_CONTACTO = "formaContacto";
+    public static final String CAMPO_ID_FORMACONTACTO = "idFormaContacto";
+    public static final String CAMPO_TIPO_FORMACONTACTO = "TipoContacto";
     public static final Integer VERSION = 4;
     public static final String NOMBREBD = "bd_ltp_usuario";
     public static final String CREAR_TABLA_USUARIO = "CREATE TABLE " + TABLA_USUARIO +
@@ -15,9 +20,9 @@ public class Utilidades {
             + CAMPO_NOMBRE + " TEXT, " +
             CAMPO_TELEFONO + " TEXT, " +
             CAMPO_ID_FORMA_CONTACTO + " INTEGER)";
-    public static final String CREAR_TABLA_FORMA_CONTACTO = "CREATE TABLE formaContacto (idFormaContacto INTEGER, TipoContacto TEXT)";
+    public static final String CREAR_TABLA_FORMA_CONTACTO = "CREATE TABLE " + TABLA_FORMA_CONTACTO + " (" + CAMPO_ID_FORMACONTACTO + " INTEGER, " +  CAMPO_TIPO_FORMACONTACTO + " TEXT)";
 
-    public static final String insert_FORMA_CONTACTO ="insert into formaContacto (idFormaContacto, TipoContacto) " +
+    public static final String insert_FORMA_CONTACTO ="INSERT INTO " + TABLA_FORMA_CONTACTO + " (" + CAMPO_ID_FORMACONTACTO + ", " + CAMPO_TIPO_FORMACONTACTO + ") " +
             "values(1, 'WhatsApp'), (2, 'Instagram'), " +
             "(3, 'Facebook'), (4, 'Linkedin');";
 }
